35981665d9e9710148e83932a858ac614adbdf0f
kondalukode <88265164+kondalukode@users.noreply.github.com>
2021-08-11 13:06:59 +0530

    Update s3Bucket.tf
    This is To Create S3 Bucket using Terraform.

--- s3Bucket.tf
+++ s3Bucket.tf
@@ -1,1 +1,17 @@
 
+provider "aws" {
+
+	region = "us-east-1"
+
+}
+
+resouce "aws_s3_bucket" "bucketK" {
+
+	bucket   = "kondalu-aws-bucket-147"
+	acl      = "private"
+
+	tags     = {
+	   Name = "My Bucket"
+	   Environment = "Dev"
+   }
+}
